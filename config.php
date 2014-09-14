dc4af78a0eb8c2cf628ac31f0184b012c68fa905
Liam Burke <hello@burk.ie>
2014-09-14 15:31:22 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?
-$host = "internal-db.s399.gridserver.com";
-$user = "db399_burkie";
-$pass = "Thebig30!";
-$db   = "db399_rota";
+$host = "localhost";
+$user = "username";
+$pass = "password";
+$db   = "database";
 
 mysql_connect($host,$user,$pass) or die(mysql_error());
 mysql_select_db($db);
